57a8bcb9c3f4c0bbfae4c29731a677abb1352e31
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2024-02-28 10:51:56 +0800

    Update curry.rs

--- .notes/curryings/Rust/curry.rs
+++ .notes/curryings/Rust/curry.rs
@@ -33,3 +33,19 @@ macro_rules! curry
 		-> $rendre { $body } 
 	} ;
 ) ;
+
+// use like
+fn main () 
+{
+	assert_eq! (4, curry! (|a, b, c, d| a + b - c - d) (3) (4) (1) (2));
+	assert_eq! (4, curry! (|a: u8, b: u8, c: u8, d: u8| a + b - c - d) (3) (4) (1) (2));
+	assert_eq! (4, curry! (|a: u8, b: u8, c: u8, d: u8| { let x = a + b - c - d ; x }) (3) (4) (1) (2));
+	assert_eq! (4, curry! (|a: u8, b: u8, c: u8, d: u8| -> u8 { a + b - c - d }) (3) (4) (1) (2));
+	
+	// let add = curry! (|a: u8, b: u8| -> u8 { a + b }) ;
+	// assert_eq! (7, curry! (|f, a, b| f(a)(b)) (add) (3) (4)); //// error: cannot infer type
+	
+	let idiot: fn(u8) -> u8 = |a: u8| -> u8 {a} ;
+	// assert_eq! (7, curry! (|a, b, c| c(a) + c(b)) (3) (4) (idiot)); //// error: cannot infer type
+	assert_eq! (7, curry! (|a: u8, b: u8, c: fn(u8) -> u8| c(a) + c(b)) (3) (4) (idiot)); //// won't error ...
+}
