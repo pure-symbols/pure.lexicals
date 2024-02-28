8009650ca2a0ca5ff62d53ccc17b1c2447be46c0
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2024-02-28 19:05:46 +0800

    Update curry.py

--- .notes/curryings/Python/curry.py
+++ .notes/curryings/Python/curry.py
@@ -5,3 +5,15 @@ curry = lambda func: fixedpoint (lambda curried:
 	(len (args) != func.__code__.co_argcount) else 
 	func (*args)) ;
 
+"""
+# e.g. def
+@curry
+def prod3 (x, y, z):
+	return x + y + z
+
+print (prod3 (3) (4) (5)) # 12
+
+# e.g. lambda
+prod_3 = curry (lambda x,y,z: x + y + z)
+print (prod_3 (6) (7) (8)) # 21
+"""
