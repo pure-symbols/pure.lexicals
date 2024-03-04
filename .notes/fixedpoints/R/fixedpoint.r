216c87917361895952bb41f3a9b3b40de274cef9
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2024-03-04 08:43:29 +0800

    Update fixedpoint.r

--- .notes/fixedpoints/R/fixedpoint.r
+++ .notes/fixedpoints/R/fixedpoint.r
@@ -4,13 +4,13 @@
 
 fixedpoint <- \ (f) (\ (x) (x) (x)) (\ (y) (f) ((y) (y))) # y-combinator.
 
-fixedpoint <- \ (f) (\ (x) (x) (x))(\ (y) (f) (\ (a) (y) (y) (a))) # z-combinator.
-fixedpoint <- \ (f) (\ (x) (x) (x))(\ (y) (f) (\ (...) (y) (y) (...))) # z-combinator also.
+fixedpoint <- \ (f) (\ (x) (x) (x)) (\ (y) (f) (\ (a) (y) (y) (a))) # z-combinator.
+fixedpoint <- \ (f) (\ (x) (x) (x)) (\ (y) (f) (\ (...) (y) (y) (...))) # z-combinator also.
+
+#' use
 
 fac <- fixedpoint (\ (f) \ (n) if (n<2) 1 else n*f(n-1))
-
 fac(9) # [1] 362880
 
 fib <- fixedpoint (\ (f) \ (n) if (n <= 1) n else f(n-1) + f(n-2))
-
 fib(9) # [1] 34
