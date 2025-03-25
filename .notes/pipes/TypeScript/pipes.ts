d1bd867d4d5ab00d3349de16a0cb6479c83e1332
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2025-03-25 23:32:51 +0800

    update pipe in note

--- .notes/pipes/TypeScript/pipes.ts
+++ .notes/pipes/TypeScript/pipes.ts
@@ -4,7 +4,14 @@
   * 
   */
 
+
+/* Depends */
+
 type Fn <T, R> = (x: T) => R ;
+
+
+
+/* Pipe */
 
 type pipe = <T,> (x: T) => <R,> (f: Fn <T, R>) => R ;
 
@@ -14,14 +21,51 @@ const pipe: pipe =
 	
 	(f) (x) ;
 
-type Pipeyard <T> = <R,> (continuation: Fn <T, R>) => Pipeyard <R> ;
 
-const Pipeyard = 
+
+/* Pipeline */
+
+type Pipeyard <T> = <R,> (continuation: Fn <T, R>) => Pipeyard <R> ;
+type pipeline = <T,> (head: T) => Pipeyard <T> ;
+
+const pipeline: pipeline = 
 <T,> (p: T): Pipeyard <T> => 
 <R,> (continuation: Fn <T, R>): Pipeyard <R> => 
 	
-	Pipeyard ((continuation) (p)) ;
+	pipeline ((continuation) (p)) ;
 
-type pipeline = <T,> (head: T) => Pipeyard <T> ;
-const pipeline: pipeline = Pipeyard as pipeline ;
+const Pipeyard = 
+<T,> (head: T)
+: Pipeyard <T> => 
+	
+	( <R,> (continuation: Fn <T, R>)
+	: Pipeyard <R> => 
+		
+		pipe (continuation (head)) (Pipeyard) 
+	
+	) as Pipeyard <T> ;
 
+Pipeyard.__broadcast = 
+<T,> (self: Pipeyard<T>)
+: T => 
+{
+	let message: T = {} as T;
+	self (x => message = x);
+	return message ;
+} ;
+
+
+
+/* try */
+
+const add = (b) => (a) => a + b;
+pipeline (42) 
+	(add (3))
+	(add (4))
+	(console.log); //> 49
+
+pipeline (6)
+	(a => a + 1)
+	(b => b * 2)
+	(c => c - 3)
+	(console.log); //> 11
