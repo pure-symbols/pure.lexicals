ec44b8bc9517e0ea9859ee111cdd42baad0639fb
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2025-04-16 01:53:41 +0800

    add as _ and pi s

--- .notes/primes/Rust/prime-iter.rs
+++ .notes/primes/Rust/prime-iter.rs
@@ -65,7 +65,7 @@ fn main ()
 	{
 		let fib = iter::iterador::unfold((0, 1), |(a, b)| (a, (b, a + b)));
 		
-		use iter::iterador::Iterador;
+		use iter::iterador::Iterador as _;
 		if let Some((head, tail)) = fib.pair() // <_ as iter::iterador::Iterador>::pair(&fib)
 		{
 			println!("3: head: {}", head); //> 3: head: 0
@@ -74,5 +74,53 @@ fn main ()
 			println!("3: fib 13: {:?}", fib.take(13).collect::<Vec<_>>());
 			//> 3: fib 13: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
 		}
+	}
+	
+	{
+		let pi_leib0 = iter::iterador::unfold(0, |k| (4.0 * (-1_f64).powi(k as i32) / (2 * k + 1) as f64, k + 1));
+		let pi_leibn = pi_leib0.take(1024*1024).sum::<f64>();
+		
+		println!("pi: pi_leibn: {}", pi_leibn); //> pi: pi_leibn: 3.141591699915466
+		
+		let pi_leib = iter::iterador::unfold(
+			(0, 0.0), 
+			|(k, tail)| (|head| (head, (k + 1, head)))
+				(tail + (4.0 * (-1_f64).powi(k as i32) / (2 * k + 1) as f64)));
+		println!("pi: pi_leib: {:?}", pi_leib.take(5).collect::<Vec<_>>());
+		//> pi: pi_leib: [4.0, 2.666666666666667, 3.466666666666667, 2.8952380952380956, 3.3396825396825403]
+		
+		let pi_gale = iter::iterador::unfold(
+			(1_f64, 1_f64 / 2_f64.sqrt(), 0.25_f64, 1_f64), 
+			|(a, b, t, p)| ( |aa| (
+				(a + b).powi(2) / (4.0 * t), 
+				(
+					aa, 
+					(a * b).sqrt(), 
+					t - p * ((a - aa) as f64).powi(2), 
+					p * 2.0)))((a + b) / 2.0));
+		println!("pi: pi_gale: {:?}", pi_gale.take(5).collect::<Vec<_>>());
+		//> pi: pi_gale: [2.914213562373095, 3.1405792505221686, 3.141592646213543, 3.141592653589794, 3.141592653589794]
+		
+		let pi_liuhui = iter::iterador::unfold(
+			(6_u64, 1_f64, 1_f64), 
+			|(n, l, r)| ( |hp| (
+				(n as f64) * l / (r * 2.0), 
+				(
+					n * 2, 
+					((r - (r.powi(2) - hp as f64).sqrt()).powi(2) + hp as f64).sqrt(), 
+					r)
+			))((l / 2.0).powi(2)));
+		println!("pi: pi_liuhui: {:?}", pi_liuhui.take(5).collect::<Vec<_>>());
+		//> pi: pi_liuhui: [3.0, 3.105828541230249, 3.1326286132812378, 3.1393502030468667, 3.14103195089051]
+		
+		// let pi_zuchongzhi = iter::iterador::unfold(
+		//     ((6.0 * 1.0) as f64, 6.0 * 3.0_f64.sqrt(), 0_u32), 
+		//     |(a, b, i)| (
+		//         (4.0 * (a + b)) / 5.0, 
+		//         (
+		//             2.0 * a * b / (a + b), 
+		//             (a * b).sqrt(), 
+		//             i + 1)));
+		// println!("pi: pi_zuchongzhi: {:?}", pi_zuchongzhi.take(5).collect::<Vec<_>>());
 	}
 }
