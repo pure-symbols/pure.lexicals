0383ad28b0c1bc55982f59f612b994176de320f7
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2026-06-12 16:18:06 +0800

    Update ns-yard.py

--- .notes/namespaces/Python/ns-yard.py
+++ .notes/namespaces/Python/ns-yard.py
@@ -1,1 +1,19 @@
-yard = lambda dic: type ('dic_yard', (), dic) ()
+
+# ── define ──────────────────────────────────────
+ns_dic = lambda dic: type ('dict_as_ns', (), dic) ()
+yard = lambda dic: ns_dic(dict(dic))
+
+# ── demo ────────────────────────────────────────
+yrd = yard(
+	a = lambda echoes: 
+		1, 
+	b = lambda echoes: 
+		echoes.a() + 1, 
+	fnc = lambda echoes: 
+		lambda x, y: x * y + echoes.a() + echoes.b(), 
+	_ = ...)
+
+# ── effect ──────────────────────────────────────
+print((yrd.a(), yrd.b(), yrd.fnc()(60, 2))) #> (1, 2, 123)
+
+
