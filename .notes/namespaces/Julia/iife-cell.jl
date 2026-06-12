2b81f7002976c4e5c047405a411397571ccb7a93
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2026-06-12 23:32:42 +0800

    Create iife-cell.jl

---
+++ .notes/namespaces/Julia/iife-cell.jl
@@ -0,0 +1,12 @@
+cell = 
+( () -> begin 
+	
+	a = 1 ;
+	b = a + 1 ;
+	c = (n) -> a + b * n ;
+
+( a = a, c = c ) end )()
+
+cell.a #> 1
+cell.c(1) #> 3
+cell.b #!> ERROR: type NamedTuple has no field b
