6668b2239d962fae0f208f3f307c6ca9ed248439
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2026-06-21 23:09:52 +0800

    Update cross.sh

--- .notes/sh3rrs/Cross/cross.sh
+++ .notes/sh3rrs/Cross/cross.sh
@@ -2,51 +2,47 @@
 
 Cross ()
 {
-	{ x_Return () { return "$1" ; } && export -f -- x_Return ; } && 
-	{ x_Bool () { ! x_Return "$1" ; } && export -f -- x_Bool ; } && 
+	x_Return () { return "$1" ; } && # export -f -- x_Return && 
+	x_Bool () { ! x_Return "$1" ; } && # export -f -- x_Bool && 
+	x_If () 
 	{ 
-		x_If () 
-		{ 
-			x_Bool "${COND:-0}" && 
-			{ "$@" ; return $? ; } || 
-			{ eval "$(cat -)" ; return $? ; } ;
-		} && 
-		export -f -- x_If ;
+		x_Bool "${COND:-0}" && 
+		{ "$@" ; return $? ; } || 
+		{ eval "$(cat -)" ; return $? ; } ;
 	} && 
+	# export -f -- x_If && 
 	
 	: && 
 	
 	x_return () ( x_Return "$@" && : ) && 
 	x_bool () ( x_Bool "$@" && : ) && 
 	x_if () ( x_If "$@" && : ) && 
-	export -f -- x_return x_bool x_if && 
+	# export -f -- x_return x_bool x_if && 
 	
 	: && 
 	
+	x_Retry () 
 	{ 
-		x_Retry () 
-		{ 
-			while ! { 
-				{
-					"${@:-:}" ;
-					rtn=$? ;
-				} ;
-				
-				x_Return $rtn && 
-				(1>&"${FILE_DESCRIPTOR:-${FD:-1}}" echo :: succeed after "$((retried))" times tryed) && 
-				
-				:; } ; 
-			do 
-				(1>&"${FILE_DESCRIPTOR:-${FD:-1}}" echo :: already tryed "$((++retried))" times) && 
-				{ x_Bool "$((retried > ${MAX_TRY:-6}))" && break ; } && 
-				:; 
-			done && 
+		while ! { 
+			{
+				"${@:-:}" ;
+				rtn=$? ;
+			} ;
 			
 			x_Return $rtn && 
+			(1>&"${FILE_DESCRIPTOR:-${FD:-1}}" echo :: succeed after "$((retried))" times tryed) && 
 			
-			:;
-		} && x_retry () ( x_Retry "$@" && : ) && export -f -- x_Retry x_retry ;
-	} && 
+			:; } ; 
+		do 
+			(1>&"${FILE_DESCRIPTOR:-${FD:-1}}" echo :: already tryed "$((++retried))" times) && 
+			{ x_Bool "$((retried > ${MAX_TRY:-6}))" && break ; } && 
+			:; 
+		done && 
+		
+		x_Return $rtn && 
+		
+		:;
+	} && x_retry () ( x_Retry "$@" && : ) && # export -f -- x_Retry x_retry && 
 	
 	: && 
 	
